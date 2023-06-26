3f7a29a16905781791ddd9e738e11e394d176e3c
TabbieManywanda <achiengtabitha@gmail.com>
2023-06-26 12:54:47 +0000

    Edit

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,47 +1,30 @@
 #!/usr/bin/python3
-
-'''UTF-8 Validation'''
-
-
-def int_to_bits(nums):
-    """
-    Helper function
-    Convert ints to bits
-    """
-    for num in nums:
-        bits = []
-        mask = 1 << 8
-        while mask:
-            mask >>= 1
-            bits.append(bool(num & mask))
-        yield bits
+"""UTF-8 validation
+"""
 
 
 def validUTF8(data):
-    """
-    Takes a list of ints and returns true if the list is
-    a valid UTF-8 encoding, else returns false
-    Args:
-        data : List of ints representing possible UTF-8 encoding
-    Return:
-        bool : True or False
-    """
-    bits = int_to_bits(data)
-    for byte in bits:
-        if byte[0] == 0:
-            continue
+    """ validate utf-8 in data comming """
 
-        ones = sum(takewhile(bool, byte))
-        if ones <= 1:
-            return False
-        if ones >= 4:
-            return False
+    count = 0
 
-        for _ in range(ones - 1):
-            try:
-                byte = next(bits)
-            except StopIteration:
+    for bit in data:
+        binary = bin(bit).replace('0b', '').rjust(8, '0')[-8:]
+        if count == 0:
+            if binary.startswith('110'):
+                count = 1
+            if binary.startswith('1110'):
+                count = 2
+            if binary.startswith('11110'):
+                count = 3
+            if binary.startswith('10'):
                 return False
-            if byte[0:2] != [1, 0]:
+        else:
+            if not binary.startswith('10'):
                 return False
+            count -= 1
+
+    if count != 0:
+        return False
+
     return True
